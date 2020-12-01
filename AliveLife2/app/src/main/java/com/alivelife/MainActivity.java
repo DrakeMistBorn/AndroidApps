c0cf55f92ddaa866844b298c9c93d0e757991937
travellerwoman <63811457+mpr-7@users.noreply.github.com>
2020-12-01 21:15:31 +0100

    Update MainActivity.java

--- AliveLife2/app/src/main/java/com/alivelife/MainActivity.java
+++ AliveLife2/app/src/main/java/com/alivelife/MainActivity.java
@@ -16,16 +16,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Configure sign-in to request the user's ID, email address, and basic
+       /* // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
-                .build();
+                .build();*/
 
-        // Set the dimensions of the sign-in button.
+      /*  // Set the dimensions of the sign-in button.
         Button signInButton = findViewById(R.id.sign_in_button);
-        /*signInButton.setSize(SignInButton.SIZE_STANDARD);*/
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
 
-        findViewById(R.id.sign_in_button).setOnClickListener((View.OnClickListener) this);
+        findViewById(R.id.sign_in_button).setOnClickListener((View.OnClickListener) this);*/
+
+        Button signinBttn = findViewById(R.id.sign_in_button);
+        signinBttn.setOnClickListener((View.OnClickListener)this);
+
     }
 }
