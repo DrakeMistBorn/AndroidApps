478fe1c56505c7e34803e43c444736e5773860ce
travellerwoman <63811457+mpr-7@users.noreply.github.com>
2020-12-07 18:01:15 +0100

    working on saving state into a file 2

--- AliveLife2/app/src/main/java/com/alivelife/UserView.java
+++ AliveLife2/app/src/main/java/com/alivelife/UserView.java
@@ -10,32 +10,45 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UserView extends AppCompatActivity {
-    ImageButton redButton = findViewById(R.id.redButton);
-    Switch locationButton = findViewById(R.id.switchLocation);
-    Switch picturesButton = findViewById(R.id.switchPictures);
-    Switch audioButton = findViewById(R.id.switchAudio);
+    ImageButton redButton;
+    Switch locationButton, picturesButton, audioButton;
+    TextView txtRedButton, txtLocation, txtPictures, txtAudio;
 
-    @SuppressLint("UseCompatLoadingForDrawables")
+    @SuppressLint({"UseCompatLoadingForDrawables", "WrongViewCast"})
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_view);
 
+        //************ Settings
+
+        locationButton = findViewById(R.id.switchLocation);
+        picturesButton = findViewById(R.id.switchPictures);
+        audioButton = findViewById(R.id.switchAudio);
+        txtRedButton = (TextView)  findViewById(R.id.redButton);
+        txtLocation = (TextView)  findViewById(R.id.redButton);
+        txtPictures = (TextView)  findViewById(R.id.redButton);
+        txtAudio = (TextView) findViewById(R.id.switchAudio);
+
+        //*************
 
         //Red button settings
         //assign the image in code (or you can do this in your layout xml with the src attribute)
-
+        redButton = findViewById(R.id.redButton);
         redButton.setImageDrawable(getBaseContext().getResources().getDrawable(R.drawable.red_button));
+
 
         //set the click listener
         redButton.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View button) {
-                //saveState();
+                saveState();
+
                 //Set the button's appearance
                 button.setSelected(!button.isSelected());
 
@@ -86,15 +99,17 @@ public class UserView extends AppCompatActivity {
                 startActivity(settings_activity);
             }
         });
+
+        loadPreferences();
     }
 
-    /*private void saveState() {
+    private void saveState() {
         SharedPreferences preferences=getSharedPreferences("state", Context.MODE_PRIVATE);
 
         boolean button = redButton.isSelected(); //isSelected: true, false
-        boolean location = locationButton.isSelected();
-        boolean pictures = picturesButton.isSelected();
-        boolean audio = audioButton.isSelected();
+        boolean location = locationButton.isChecked();
+        boolean pictures = picturesButton.isChecked();
+        boolean audio = audioButton.isChecked();
 
 
         SharedPreferences.Editor editor = preferences.edit();
@@ -103,7 +118,25 @@ public class UserView extends AppCompatActivity {
         editor.putBoolean("pictures_activated", pictures);
         editor.putBoolean("audio_activated", audio);
 
-    }*/
+
+        editor.commit();
+
+
+    }
+
+    private void loadPreferences() {
+        SharedPreferences preferences=getSharedPreferences("state", Context.MODE_PRIVATE);
+
+        Boolean button = preferences.getBoolean("red_button", false);
+        Boolean location = preferences.getBoolean("location_activated", false);
+        Boolean pictures = preferences.getBoolean("pictures_activated", false);
+        Boolean audio = preferences.getBoolean("audio_button", false);
+
+        redButton.setSelected(button);
+        locationButton.setChecked(location);
+        picturesButton.setChecked(pictures);
+        audioButton.setChecked(audio);
+    }
 
 
 }
