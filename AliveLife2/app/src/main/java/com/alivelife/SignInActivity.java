6528aaa4aec75e44f7775c85935864b5c896b5a1
DrakeMistBorn <40219320+DrakeMistBorn@users.noreply.github.com>
2020-12-07 13:06:21 +0100

    R.M. MinSDK 29, Funciona login en Emulador Android 10, fallo pulsar dos veces boton login solucionado

--- AliveLife2/app/src/main/java/com/alivelife/SignInActivity.java
+++ AliveLife2/app/src/main/java/com/alivelife/SignInActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.LogPrinter;
 import android.view.View;
+import android.webkit.ConsoleMessage;
 import android.widget.ImageButton;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -17,6 +19,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseUser;
 
 public class SignInActivity extends AppCompatActivity {
+    private String etiqueta = "Estoy en: ";
 
     //### START [     Google OAuth2 variables     ]
     private static final int RC_SIGN_IN = 1;
@@ -28,14 +31,14 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
         addListenerOnButton();
+        if(GoogleSignIn.getLastSignedInAccount(this) != null ) goToSecondActivity();    //###   Log in directly if already logged
 
         //### START  [     Google OAuth2: request users ID and basic profile information     ]
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
-        
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);        //### Google Sign In Client.
 
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);        //### Google Sign In Client.
         //### END    [     Google OAuth2: request users ID and basic profile information     ]
     }
 
@@ -43,11 +46,18 @@ public class SignInActivity extends AppCompatActivity {
     @Override
     protected void onStart(){
         super.onStart();
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        //GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
         //updateUI(account);
     }
-
     //### END       [     onStart method:     ]
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(GoogleSignIn.getLastSignedInAccount(this) != null) goToSecondActivity();
+        //GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        //updateUI(account);
+    }
 
     //### START     [     Sign In listener button:  Send to Gmail login when triggered    ]
     public void addListenerOnButton() {
@@ -64,14 +74,18 @@ public class SignInActivity extends AppCompatActivity {
 
 
     public void signIn(View v) {
-        Intent i = new Intent(SignInActivity.this, UserView.class);
-        startActivity(i);
-
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);       //###     Request code
-
-
+        Log.d(etiqueta, "Antes del IF ");
+        System.out.println("Estoy en el print antes del IF *******************************************************************");
+        if(GoogleSignIn.getLastSignedInAccount(this) != null) goToSecondActivity();
     }
 
+    //### START     [     Go to the second activity if Gmail login was successful    ]
+    public void goToSecondActivity(){
+        Intent i = new Intent(SignInActivity.this, UserView.class);
+        startActivity(i);
+    }
+    //### END       [     Go to the second activity if Gmail login was successful    ]
 
 }
