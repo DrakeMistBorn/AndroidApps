cd60cf9966e7b831a49283f91acfc40f6e69e517
travellerwoman <63811457+mpr-7@users.noreply.github.com>
2020-12-07 12:46:49 +0100

    working on saving state into a file

--- AliveLife2/app/src/main/java/com/alivelife/ProtectorView.java
+++ AliveLife2/app/src/main/java/com/alivelife/ProtectorView.java
@@ -32,6 +32,17 @@ public class ProtectorView extends AppCompatActivity {
     }
 
     @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        // Restore UI state from the savedInstanceState.
+        // This bundle has also been passed to onCreate.
+        boolean myBoolean = savedInstanceState.getBoolean("MyBoolean");
+        double myDouble = savedInstanceState.getDouble("myDouble");
+        int myInt = savedInstanceState.getInt("MyInt");
+        String myString = savedInstanceState.getString("MyString");
+    }
+
+    @Override
     protected void onStart(){
         super.onStart();
         Log.d(etiqueta, "onStart: ");
