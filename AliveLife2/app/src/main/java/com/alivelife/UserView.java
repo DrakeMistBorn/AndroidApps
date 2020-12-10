faff381d45bca5b2b274529114ee70180ed8428d
travellerwoman <63811457+mpr-7@users.noreply.github.com>
2020-12-11 00:57:53 +0100

    Notification

--- AliveLife2/app/src/main/java/com/alivelife/UserView.java
+++ AliveLife2/app/src/main/java/com/alivelife/UserView.java
@@ -2,21 +2,31 @@
 package com.alivelife;
 
 import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import java.io.File;
+import java.util.Collections;
 
 public class UserView extends AppCompatActivity {
     ImageButton redButton;
     Switch locationButton, picturesButton, audioButton;
+    private NotificationUtils mNotificationUtils;
     //TextView txtRedButton, txtLocation, txtPictures, txtAudio;
 
     @Override
@@ -31,10 +41,11 @@ public class UserView extends AppCompatActivity {
         locationButton = findViewById(R.id.switchLocation);
         picturesButton = findViewById(R.id.switchPictures);
         audioButton = findViewById(R.id.switchAudio);
+        mNotificationUtils = new NotificationUtils(this);
 
         //*************
 
-        //Red button settings
+        //*************** Red button settings
         //assign the image in code (or you can do this in your layout xml with the src attribute)
         redButton = findViewById(R.id.redButton);
         redButton.setImageDrawable(getBaseContext().getResources().getDrawable(R.drawable.red_button));
@@ -50,11 +61,14 @@ public class UserView extends AppCompatActivity {
                 button.setSelected(!button.isSelected());
                 changeButtonColor (button);
 
+                //notification settings
+                Notification.Builder nb = mNotificationUtils.getAndroidChannelNotification("Candy Splash", "Play today to maintain your streak");
+                mNotificationUtils.getManager().notify(101, nb.build());
             }
 
         });
 
-
+        //***************
 
 
 
@@ -91,7 +105,6 @@ public class UserView extends AppCompatActivity {
             public void onClick (View pv) {
                 saveState();
                 Intent settings_activity = new Intent(UserView.this, ProtectorView.class);
-                startActivity(settings_activity);
             }
         });
 
@@ -120,7 +133,7 @@ public class UserView extends AppCompatActivity {
         editor.commit();
     }
 
-    private void loadPreferences() {
+    public void loadPreferences() {
         SharedPreferences preferences=getSharedPreferences("state", Context.MODE_PRIVATE);
 
         Boolean button = preferences.getBoolean("red_button", false);
@@ -141,6 +154,4 @@ public class UserView extends AppCompatActivity {
             redButton.setImageDrawable(getBaseContext().getResources().getDrawable(R.drawable.red_button));
         }
     }
-
-
 }
